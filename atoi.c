7513dbc3bc0d084818c090da5f3c94bdcc490f9b
Oussama Mlouk <omlouk@c2r1s11.42beirut.com>
2025-11-17 15:33:39 +0200

    Added new files atoi, memcmp, strnstr but still needs modification

---
+++ atoi.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   atoi.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omlouk <omlouk@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 15:19:09 by omlouk            #+#    #+#             */
+/*   Updated: 2025/11/17 15:23:07 by omlouk           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_isspace(char c)
+{
+	if (c == 32 || (c >= 9 && c <= 13) || c == 160)
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(char *str)
+{
+	int	nb;
+	int	i;
+	int	flag;
+
+	flag = 1;
+	nb = 0;
+	i = 0;
+	while (ft_isspace(str[i]) && str[i] != '\0')
+	{
+		i++;
+	}
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			flag *= -1;
+		i++;
+	}
+	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
+	{
+		nb = nb * 10 + (str[i] - '0');
+		i++;
+	}
+	return (flag * nb);
+}
---
+++ strnstr.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strnstr.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omlouk <omlouk@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 15:19:23 by omlouk            #+#    #+#             */
+/*   Updated: 2025/11/17 15:31:10 by omlouk           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+char	*strnstr(const char *big, const char *little, size_t len);
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (!to_find[i])
+		return (str);
+	while (str[i])
+	{
+		j = 0;
+		if (to_find[j] == str[i])
+		{
+			while (str[i + j] && to_find[j] && to_find[j] == str[i + j])
+			{
+				j++;
+			}
+		}
+		if (j == ft_strlen(to_find))
+			return (str + i);
+		else
+			i++;
+	}
+	return (0);
+}
